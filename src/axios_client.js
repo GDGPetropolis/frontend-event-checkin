e41db4b727c853c78ef88d74d626d9902c4da403
John Fercher <johnathanfercher22@gmail.com>
2019-03-25 14:39:07 -0300

    aumentado timeout

--- src/axios_client.js
+++ src/axios_client.js
@@ -3,7 +3,7 @@ import backend from './backend'
 
 export default axios.create({
     baseURL: backend,
-    timeout: 5000,
+    timeout: 10000,
     headers: {
         'Access-Control-Allow-Origin': '*'
     }
