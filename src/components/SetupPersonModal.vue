815e833a7cc6dac5a41e75dc0e3c88fdfe342623
John Fercher <johnathanfercher22@gmail.com>
2019-03-29 13:55:09 -0300

    Adicionado loading em SetuoPersonModal

--- src/components/SetupPersonModal.vue
+++ src/components/SetupPersonModal.vue
@@ -1,6 +1,6 @@
 <template>
     <b-modal ref="modalPerson" size="lg" hide-footer title="Setup de Participante">
-        <b-row>
+        <b-row v-if="loaded">
             <b-col md="5">
                 <b-input id="inlineFormInputName" placeholder="Informe o Nome..." v-model="person.name"/>
             </b-col>
@@ -11,15 +11,23 @@
                 <b-button variant="primary" v-on:click="doModalPersonSetup">Fazer Setup</b-button>
             </b-col>
         </b-row>
+        <b-row v-else>
+            <Loading/>
+        </b-row>
     </b-modal>
 </template>
 
 <script>
     import axios_client from "./../axios_client";
+    import Loading from "./../components/Loading.vue"
 
     export default {
+        components: {
+            Loading
+        },
         data () {
             return {
+                loaded: false,
                 person: {
                     id: null,
                     name: null,
@@ -31,13 +39,20 @@
             async doModalPersonSetup(){
                 await axios_client.put("/api/person", this.person);
                 this.$emit('personSetupDone', this.id);
+
                 this.$refs.modalPerson.hide();
             },
-            showModal(id) {
-                this.person.id = id;
-                this.person.name = "";
-                this.person.email = "";
+            async showModal(id) {
+                this.loaded = false;
+
                 this.$refs.modalPerson.show();
+                var response = await axios_client.get("/api/person?id=" + id);
+
+                this.person.id = id;
+                this.person.name = response.data.name;
+                this.person.email = response.data.email;
+
+                this.loaded = true;
             }
         }
     }
